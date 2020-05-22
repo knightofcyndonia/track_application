82846da560c80f4d93f67a2742d13849d03a785d
knightofcyndonia <knightofcyndonia@gmail.com>
2020-05-22 14:00:49 +0700

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Component, ViewChild, ElementRef, OnInit  } from '@angular/core';
-=======
-import { Component, ViewChild, ElementRef } from '@angular/core';
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
 
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
@@ -26,15 +22,10 @@ export class HomePage {
   locationsCollection: AngularFirestoreCollection<any>;
   user = null;
 
-<<<<<<< HEAD
   lastLocation :any = [];
 
   @ViewChild('map') mapElement: ElementRef;
   maps: any;
-=======
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
   markers = []; 
 
   isTracking = false;
@@ -52,7 +43,6 @@ export class HomePage {
   }
 
   anonLogin(){
-<<<<<<< HEAD
     console.log("masuk anon login");
     this.afAuth.signInAnonymously().then(res =>{
       this.user = res.user;
@@ -64,17 +54,6 @@ export class HomePage {
       )
 
 
-=======
-    this.afAuth.signInAnonymously().then(res =>{
-      console.log(res);
-      this.user = res.user;
-
-      this.locationsCollection = this.afs.collection(
-        `locations/${this.user.uid}/track`,
-        ref => ref.orderBy('timestamp')
-      )
-
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
       //load firebase data
       // this.locations = this.locationsCollection.valueChanges();
 
@@ -106,16 +85,11 @@ export class HomePage {
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
  
-<<<<<<< HEAD
     this.maps = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-=======
-    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
   }
 
   // Use Capacitor to track our geolocation
 startTracking() {
-<<<<<<< HEAD
   console.log("Start Tracking");
 
   let busName = "BUS 1";
@@ -160,22 +134,16 @@ startTracking() {
   })
 
   let timeRemaining = "";
-=======
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
   this.isTracking = true;
   this.watch = Geolocation.watchPosition({}, (position, err) => {
     if (position) {
       this.addNewLocation(
         position.coords.latitude,
         position.coords.longitude,
-<<<<<<< HEAD
         position.timestamp,
         this.user.uid,
         busName,
         timeRemaining
-=======
-        position.timestamp
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
       );
     }
   });
@@ -186,7 +154,6 @@ stopTracking() {
   Geolocation.clearWatch({ id: this.watch }).then(() => {
     this.isTracking = false;
   });
-<<<<<<< HEAD
   
 }
  
@@ -211,36 +178,13 @@ addNewLocation(lat, lng, timestamp, userId, bus_name, time_remaining) {
   let position = new google.maps.LatLng(lat, lng);
   this.maps.setCenter(position);
   this.maps.setZoom(15);
-=======
-}
- 
-// Save a new location to Firebase and center the map
-addNewLocation(lat, lng, timestamp) {
-  
-  console.log("husein postition");
-  console.log(lat);
-  console.log(lng);
-  console.log(timestamp);
-  this.locationsCollection.add({
-    lat,
-    lng,
-    timestamp
-  });
- 
-  let position = new google.maps.LatLng(lat, lng);
-  this.map.setCenter(position);
-  this.map.setZoom(5);
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
 }
  
  
 // Redraw all markers on the map
 updateMap(locations) {
-<<<<<<< HEAD
   
   console.log("masuk update map");
-=======
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
   // Remove all current marker
   this.markers.map(marker => marker.setMap(null));
   this.markers = [];
@@ -249,32 +193,21 @@ updateMap(locations) {
     let latLng = new google.maps.LatLng(loc.lat, loc.lng);
  
     let marker = new google.maps.Marker({
-<<<<<<< HEAD
       map: this.maps,
       animation: google.maps.Animation.DROP,
       position: latLng
     });
     this.markers.push(marker);
     this.lastLocation = loc;
-=======
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: latLng
-    });
-    // this.markers.push(marker);
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
   }
 }
 
 // Delete a location from Firebase
 deleteLocation(pos) {
   // console.log('delete : ', pos);
-<<<<<<< HEAD
   
   console.log("masuk delete location");
   console.log(pos)
-=======
->>>>>>> e615d571d4f64a3f18490296d80e83375d4edfb0
   this.locationsCollection.doc(pos.id).delete();
 }
 
